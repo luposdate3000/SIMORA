32f7d283019a343e1ed6da64cd2d5103bd58bbad
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-12-15 10:40:27 +0000

    add script to aggregate benchmark results

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 routing=( \
-./resources/routing/Application_RPLFast.json \
-./resources/routing/NoMulticast_RPLFast.json \
-./resources/routing/RoutingAndApplication_RPLFast.json \
-./resources/routing/Routing_RPLFast.json \
+./resources/routing/ApplicationRPLFast.json \
+./resources/routing/NoMulticastRPLFast.json \
+./resources/routing/RoutingAndApplicationRPLFast.json \
+./resources/routing/RoutingRPLFast.json \
 )
 scenarios=( \
 ./resources/scenarios/mail.json \
 )
 topologies=( \
-./resources/topologies/Uniform64.json \
-./resources/topologies/Uniform32.json \
 ./resources/topologies/Full16.json \
 ./resources/topologies/Full32.json \
 ./resources/topologies/Full4.json \
@@ -27,7 +25,9 @@ topologies=( \
 ./resources/topologies/Ring64.json \
 ./resources/topologies/Ring8.json \
 ./resources/topologies/Uniform16.json \
+./resources/topologies/Uniform32.json \
 ./resources/topologies/Uniform4.json \
+./resources/topologies/Uniform64.json \
 ./resources/topologies/Uniform8.json \
 )
 
@@ -38,15 +38,17 @@ do
 for t in ${topologies[@]}
 do
 echo
+pkill java -9
 echo java -cp $(cat ./build/external_jvm_dependencies | tr "\n" ":"):./build/libs/simora-jvm-0.0.1.jar simora.MainKt $r $s $t jvm.json
-/usr/bin/time -v java -cp $(cat ./build/external_jvm_dependencies | tr "\n" ":"):./build/libs/simora-jvm-0.0.1.jar simora.MainKt $r $s $t jvm.json
-echo
-echo ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
-/usr/bin/time -v ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
+x=$(/usr/bin/time -o tmp -v java -cp $(cat ./build/external_jvm_dependencies | tr "\n" ":"):./build/libs/simora-jvm-0.0.1.jar simora.MainKt $r $s $t jvm.json | grep simulator_output | sed "s/.*outputdirectory=//g")
+mv tmp "$x/time"
+exit
+#echo
+#echo ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
+#/usr/bin/time -v ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
 #echo
 #echo ./build/bin/linuxX64/debugExecutable/simora.kexe $r $s $t linux.json
 #/usr/bin/time -v ./build/bin/linuxX64/debugExecutable/simora.kexe $r $s $t linux.json
-exit
 done
 done
 done
---
+++ benchmark_sumary.main.kts
@@ -0,0 +1,64 @@
+#!/usr/bin/env kotlin
+import java.io.File
+
+val argumentNames = mutableListOf<String>()
+val rows = mutableListOf<MutableList<String>>()
+
+
+fun addRow(row: Map<String, String>) {
+    for (k in row.keys) {
+        if (!argumentNames.contains(k)) {
+            argumentNames.add(k)
+        }
+    }
+    var r = MutableList<String>(argumentNames.size) { "" }
+    for ((k, v) in row) {
+        r[argumentNames.indexOf(k)] = v
+    }
+    rows.add(r)
+}
+
+for (f in File("simulator_output").walk().maxDepth(1)) {
+    val filename = f.toString()
+    if (filename != "simulator_output") {
+        val x = filename.substring(filename.lastIndexOf("/") + 2).split("_")
+        var headers: List<String>? = null
+        val baseRow = mutableMapOf<String, String>()
+        baseRow["routing"] = x[0]
+        baseRow["scenario"] = x[1]
+        baseRow["topology"] = x[2]
+        baseRow["platform"] = x[3]
+        File(filename + "/time").forEachLine { line ->
+if(line.contains("User time (seconds)")){
+baseRow["system time (Seconds)"]=line.substring(line.lastIndexOf(":")+2)
+}else if(line.contains("Maximum resident set size (kbytes)")){
+baseRow["system memory (Bytes)"]=(line.substring(line.lastIndexOf(":")+2).toLong()*1024L).toString()
+}else if(line.contains("Minor (reclaiming a frame) page faults")){
+baseRow["system page faults"]=line.substring(line.lastIndexOf(":")+2)
+}else if(line.contains("Voluntary context switches")){
+baseRow["system context switches"]=line.substring(line.lastIndexOf(":")+2)
+}
+        }
+        File(filename + "/measurement.csv").forEachLine { line ->
+            val l = line.split(",")
+            val h = headers
+            if (h == null) {
+                headers = l
+            } else {
+                val row = mutableMapOf<String, String>()
+                row.putAll(baseRow)
+                val s = if (h.size < l.size) h.size else l.size
+                for (i in 0 until s) {
+                    row[h[i]] = l[i]
+                }
+                addRow(row)
+            }
+        }
+    }
+}
+
+
+println(argumentNames.joinToString())
+for (r in rows) {
+    println(r.joinToString() + ",".repeat(argumentNames.size - r.size))
+}
