7c34fcff0741799f9e520a9a5a2d095905425144
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-12-15 10:46:32 +0000

    add very large topologies for strong scaling test

--- benchmark.sh
+++ benchmark.sh
@@ -1,54 +1,34 @@
 #!/bin/bash
-routing=( \
-./resources/routing/ApplicationRPLFast.json \
-./resources/routing/NoMulticastRPLFast.json \
-./resources/routing/RoutingAndApplicationRPLFast.json \
-./resources/routing/RoutingRPLFast.json \
-)
+routing=( $(find ./resources/routing/ -name *.json | sort))
+topologies=($(find ./resources/topologies/ -name *.json | sort))
 scenarios=( \
 ./resources/scenarios/mail.json \
 )
-topologies=( \
-./resources/topologies/Full16.json \
-./resources/topologies/Full32.json \
-./resources/topologies/Full4.json \
-./resources/topologies/Full64.json \
-./resources/topologies/Full8.json \
-./resources/topologies/Random16.json \
-./resources/topologies/Random32.json \
-./resources/topologies/Random4.json \
-./resources/topologies/Random64.json \
-./resources/topologies/Random8.json \
-./resources/topologies/Ring16.json \
-./resources/topologies/Ring32.json \
-./resources/topologies/Ring4.json \
-./resources/topologies/Ring64.json \
-./resources/topologies/Ring8.json \
-./resources/topologies/Uniform16.json \
-./resources/topologies/Uniform32.json \
-./resources/topologies/Uniform4.json \
-./resources/topologies/Uniform64.json \
-./resources/topologies/Uniform8.json \
-)
-
+for t in ${topologies[@]}
+do
 for r in ${routing[@]}
 do
 for s in ${scenarios[@]}
-do
-for t in ${topologies[@]}
 do
 echo
 pkill java -9
 echo java -cp $(cat ./build/external_jvm_dependencies | tr "\n" ":"):./build/libs/simora-jvm-0.0.1.jar simora.MainKt $r $s $t jvm.json
 x=$(/usr/bin/time -o tmp -v java -cp $(cat ./build/external_jvm_dependencies | tr "\n" ":"):./build/libs/simora-jvm-0.0.1.jar simora.MainKt $r $s $t jvm.json | grep simulator_output | sed "s/.*outputdirectory=//g")
 mv tmp "$x/time"
+done
+done
+done
+for t in ${topologies[@]}
+do
+for r in ${routing[@]}
+do
+for s in ${scenarios[@]}
+do
 exit
-#echo
-#echo ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
-#/usr/bin/time -v ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
-#echo
-#echo ./build/bin/linuxX64/debugExecutable/simora.kexe $r $s $t linux.json
-#/usr/bin/time -v ./build/bin/linuxX64/debugExecutable/simora.kexe $r $s $t linux.json
+echo
+echo ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json
+x=$(/usr/bin/time -o tmp -v ./build/bin/linuxX64/releaseExecutable/simora.kexe $r $s $t linux.json | grep simulator_output | sed "s/.*outputdirectory=//g")
+mv tmp "$x/time"
 done
 done
 done
